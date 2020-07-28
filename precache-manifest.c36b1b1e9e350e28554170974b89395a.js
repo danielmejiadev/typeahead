1f554ede20abf1545930ec0f10a93ab79ec73397
Daniel Mejia <danielmejia@lmejia-mac.local>
2020-07-27 21:59:45 -0500

    Updates

---
+++ precache-manifest.c36b1b1e9e350e28554170974b89395a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b0dbb1f4546574c7ee367db62705208",
+    "url": "/typeahead/index.html"
+  },
+  {
+    "revision": "9b393a04140c4e6f4195",
+    "url": "/typeahead/static/css/main.afd83791.chunk.css"
+  },
+  {
+    "revision": "6f61f05e24f800182003",
+    "url": "/typeahead/static/js/2.ae3be2eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/typeahead/static/js/2.ae3be2eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b393a04140c4e6f4195",
+    "url": "/typeahead/static/js/main.b989853c.chunk.js"
+  },
+  {
+    "revision": "4a8a8d8c0f3a138b415d",
+    "url": "/typeahead/static/js/runtime-main.ae855a5e.js"
+  }
+]);
